2a9fcbe01c96d17953471b50855bae3598c658e4
Marlon Henrique <matiasmarlon2003@gmail.com>
2024-12-12 09:47:35 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/udemy/workshomongo/resources/UserResource.java
+++ src/main/java/com/udemy/workshomongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.udemy.workshomongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.workshomongo.domain.User;
+import com.udemy.workshomongo.dto.UserDTO;
 import com.udemy.workshomongo.resources.services.UserService;
 
 @RestController
@@ -19,9 +21,12 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		
+		 // Converte a lista de User para UserDTO
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 		
 		/*
 		 		User maria = new User("1001", "Maria Brown", "maria@gmail.com");
