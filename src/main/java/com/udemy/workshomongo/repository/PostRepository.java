0a3c825c894612533fe437fd2b49f9181a4fcdf8
Marlon Henrique <matiasmarlon2003@gmail.com>
2024-12-17 09:46:26 -0300

    Consulta simples com query methods

--- src/main/java/com/udemy/workshomongo/repository/PostRepository.java
+++ src/main/java/com/udemy/workshomongo/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package com.udemy.workshomongo.repository;
 
+import java.util.List;
+
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.udemy.workshomongo.domain.Post;
-import com.udemy.workshomongo.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
